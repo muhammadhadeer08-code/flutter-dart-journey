0d1014dd93b95b20726036ca9ccfd2f4a4a469db
Muhammad Hadeer <muhammadhadeer08@gmail.com>
2026-09-25 03:38:21 -0700

    Completed first code of dart and cover basic data types and variable

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("I have printed first code of dart");
+  String name = "Hadeer";
+  int age = 21;
+  print("I am $name \nI am $age years old");
+  String email = "muhammadhadeer08@gmail.com";
+  print("My email:  $email ");
+  double interpercentage = 64.75;
+  print("My inter percentage: $interpercentage");
+  double BSCScgpa = 3.4;
+  print("My BSCS GPA: $BSCScgpa");
+  bool isstudent = true;
+  print("Is student:$isstudent");
+}
