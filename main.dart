72d5a669a2e6eda47629c0e03ebd66f20a6493d6
Muhammad Hadeer <muhammadhadeer08@gmail.com>
2026-09-25 06:50:28 -0700

    Completed Operator Challenge

--- main.dart
+++ main.dart
@@ -1,14 +1,18 @@
+// Operator Challenge
+/*
+Imagine you have 45 chocolate candies, and you want to distribute them equally among 4 friends and take input from user.
+Using Dart code, you need to find out these two things:
+How many complete candies will each friend get? (Which operator should be used for this?)
+How many candies will be left over after the distribution? (Which operator should be used for this?)*/
+
+import "dart:io";
 void main() {
-  print("I have printed first code of dart");
-  String name = "Hadeer";
-  int age = 21;
-  print("I am $name \nI am $age years old");
-  String email = "muhammadhadeer08@gmail.com";
-  print("My email:  $email ");
-  double interpercentage = 64.75;
-  print("My inter percentage: $interpercentage");
-  double BSCScgpa = 3.4;
-  print("My BSCS GPA: $BSCScgpa");
-  bool isstudent = true;
-  print("Is student:$isstudent");
+  print("Total candies:");
+  int a = int.parse(stdin.readLineSync()!);
+  print("Total friends:");
+  int b = int.parse(stdin.readLineSync()!);
+  int totalcandies = a ~/ b;
+  int leftover = a % b;
+  print("Each friend gets: $totalcandies candies.");
+  print("Left over candies: $leftover.");
 }
