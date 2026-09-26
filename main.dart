bb44473a7393ba13d0ab09b77449898397738cea
Muhammad Hadeer <muhammadhadeer08@gmail.com>
2026-09-26 11:19:06 -0700

    Completed conditional statements: added if-else and switch-case programs

--- main.dart
+++ main.dart
@@ -1,18 +1,74 @@
-// Operator Challenge
+//Write a program that takes an integer score from the user.
+//Use an if-else statement to print "Pass" if the score is 50 or greater
+//and "Fail" if it is less than 50. Provide the code when ready.
 /*
-Imagine you have 45 chocolate candies, and you want to distribute them equally among 4 friends and take input from user.
-Using Dart code, you need to find out these two things:
-How many complete candies will each friend get? (Which operator should be used for this?)
-How many candies will be left over after the distribution? (Which operator should be used for this?)*/
-
 import "dart:io";
+
 void main() {
-  print("Total candies:");
+  print("put any number:");
   int a = int.parse(stdin.readLineSync()!);
-  print("Total friends:");
-  int b = int.parse(stdin.readLineSync()!);
-  int totalcandies = a ~/ b;
-  int leftover = a % b;
-  print("Each friend gets: $totalcandies candies.");
-  print("Left over candies: $leftover.");
+  if (a >= 50) {
+    print("pass");
+  } else {
+    print("fail");
+  }
+}
+
+//Write a program that takes an integer score from the user,
+// and using an if-else if-else statement, check:
+//If the score is 80 or greater, print "Grade A".
+//If the score is 50 or greater (but less than 80), print "Grade B".
+//If the score is less than 50, print "Fail".
+import "dart:io";
+
+void main() {
+  print("Put the Score:");
+  int Score = int.parse(stdin.readLineSync()!);
+  if (Score >= 80) {
+    print("Grade=A");
+  } else if (Score >= 50 && Score <= 80) {
+    print("Grade=B");
+  } else {
+    print("fail");
+  }
+}
+*/
+/*Challenge Question:
+
+Write a Dart program that takes a day of the week as a String input from the user.
+ Using a switch-case statement:
+If the input is "Monday", "Tuesday", "Wednesday", "Thursday", or "Friday", print "This is a Working Day."
+If the input is "Saturday" or "Sunday", print "This is the Weekend!"
+If the input matches none of these valid days, use a default case to print "Invalid input! Please enter a valid day name."
+*/
+import "dart:io";
+
+void main() {
+  print("Enter the day:");
+  String weekday = stdin.readLineSync()!;
+  switch (weekday) {
+    case "Monday":
+      print("This is a working day");
+      break;
+    case "Tuesday":
+      print("This is a working day");
+      break;
+    case "Wednesday":
+      print("This a working day");
+      break;
+    case "Thursday":
+      print("This is a working day");
+      break;
+    case "Friday":
+      print("This a working day");
+      break;
+    case "saturday":
+      print("This is a weekend day");
+      break;
+    case "Sunday":
+      print("This is a weekend day");
+      break;
+    default:
+      print("Invalid input! Please enter a valid");
+  }
 }
